eec4d214beb71ce988244f2e411b26debfdb27fc
Kim Yu Na <81612540+U-and-Me@users.noreply.github.com>
2021-05-13 17:22:44 +0900

    Create Variable.js

---
+++ Variable.js
@@ -0,0 +1,12 @@
+// 2일차
+const a = 221;
+let b = a - 5;
+// a = 4;
+console.log(b, a);
+
+const what1 = "Hello😙"; //String
+console.log(what1);
+
+const what2 = true; // Boolean
+const what3 = 666; //Number
+const what4 = 55.1; //float
