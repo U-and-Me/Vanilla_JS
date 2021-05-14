350f58abfc27af1baad17017911a58e0c655e08f
Kim Yu Na <81612540+U-and-Me@users.noreply.github.com>
2021-05-14 17:26:31 +0900

    Create If_Else.js

---
+++ Day 04/If_Else.js
@@ -0,0 +1,26 @@
+// 4일차
+
+// #2.5
+if("10" === 10){
+    console.log("Hi!!");
+}else if("10" === "10"){
+    console.log("Hello!");
+}else{
+    console.log("Ho!!");
+}
+
+if(20 > 5 || "yuna" === "yuna"){
+    console.log("Yes");
+}else{
+    console.log("No");
+}
+
+const age = prompt("How old are you");
+if(age >= 18 && age <= 21){
+    console.log("You can drink but you should not");
+}else if(age > 21){
+    console.log("go ahed")
+}
+else{
+    console.log("too young")
+}
